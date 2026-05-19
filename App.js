dbaf380a83cafe958ccb09f0945e2d108fe08d4a
私•Λρεχ乂Ιηfιηιτy•ㇱ <apexinfinity243@gmail.com>
2026-05-20 00:21:35 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Salut APEX 👋</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#121212",
+  },
+
+  text: {
+    fontSize: 30,
+    color: "#00ff99",
+    fontWeight: "bold",
+  },
+});
